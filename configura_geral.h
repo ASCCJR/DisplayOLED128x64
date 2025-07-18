493c439b9a264f18dfcf53549780f9c12b5e658c
Antonio Sergio C. C. JR. <antoniosergiok@gmail.com>
2025-07-18 02:38:59 -0300

    Add files via upload

---
+++ configura_geral.h
@@ -0,0 +1,8 @@
+#ifndef CONFIGURA_GERAL_H
+#define CONFIGURA_GERAL_H
+
+// Display OLED 0.96" 128x64 com I2C
+#define SDA_PIN 14 // GPIO 14
+#define SCL_PIN 15 // GPIO 15 
+
+#endif // CONFIGURA_GERAL_H
